d912a05ef57bfec13ac0f87f4622c4a4adb45136
senpatthamawan <sen.patthamawan@gmail.com>
2023-08-18 19:41:01 +0700

    initial project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System; //ลบออกได้ เพราะเราไม่ได้ใช้ เราใช้จากจากหน้าอื่นแล้ว (จากการเรียกใช้ class)
+using static csharp_basic.ex1_hello_console;
+
+namespace csharp_basic
+{
+    class Program
+    {
+        static void Main()
+        {
+            ex1();
+
+        }
+    }
+}
---
+++ ex1_hello_console.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp_basic
+{
+    public class ex1_hello_console
+    {
+        public static void ex1(){
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("เซ็น");
+            Console.WriteLine(5*5+2);
+
+            Console.Write("ปัทมวรรณ");
+            Console.Write(" จันทร์เทพ\n\n\n");
+
+            Console.WriteLine("\twelcom" + " guys");
+            Console.WriteLine("ding-dong\a");
+        }
+    }
+}
